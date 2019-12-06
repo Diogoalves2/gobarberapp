54210a245c2d539f6c3e70e5470e034f59761b89
Diogoalves2 <diogolsm.barboza39@gmail.com>
2019-12-06 19:30:20 -0300

    Versao 0.0.1

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,72 @@
+import React, { useRef, useState } from 'react';
+import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+
+import logo from '~/assets/logo.png';
+
+import Background from '~/components/Background';
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignIn({ navigation }) {
+  const dispacth = useDispatch();
+  const passwordRef = useRef();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit() {
+    dispacth(signInRequest(email, password));
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            OnSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua Senha"
+            ref={passwordRef}
+            returnKeyType="send"
+            OnSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
+          />
+
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Acessar
+          </SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Criar Conta Gratuita</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
+}
